3831a2962bdf15d473aa8d134dd3aecb5e06b700
oleweps <weps6179@gmail.com>
2023-08-29 12:26:05 +0000

    Python - Classes and Objects

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,14 +6,13 @@ class Node:
     """Represent a node in a singly-linked list."""
 
     def __init__(self, data, next_node=None):
-    """Initialize a new Node.
-
-    Args:
-    data (int): The data of the new Node.
-    next_node (Node): The next node of the new Node.
-    """
-    self.data = data
-    self.next_node = next_node
+        """Initialize a new Node.
+        Args:
+        data (int): The data of the new Node.
+        next_node (Node): The next node of the new Node.
+        """
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
