0110b0f1f95abaaa4e71969810e3950baae1a2fa
oleweps <weps6179@gmail.com>
2023-09-06 15:36:38 +0000

    Python - More Classes and Objects

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
 
         Represents the rectangle with the # character.
         """
-        if self.__width == 0 or self.__height == 0:
+        if self.__width == 0 and self.__height == 0:
             return ("")
 
         rect = []
