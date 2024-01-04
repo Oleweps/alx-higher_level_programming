39c6d00ad3949c35358f115fe7e3fd6f2d824cf3
Oleweps <weps6179@gmail.com>
2024-01-04 09:51:31 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,20 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+// Check if the file path is provided as an argument
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file-path>');
+  process.exit(1);
+}
+const filePath = process.argv[2];
+
+// Read the content of the file in utf-8
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+// Print the error object if an error occurred during reading
+    console.error(err);
+  } else {
+// Print the content of the file
+    console.log(data);
+  }
 });
